e82ac554871d7e6fecda9d85c5e744bd91a58725
pinkissocool <99200272+pinkissocool@users.noreply.github.com>
2022-12-16 14:04:10 -0500

    Add files via upload

--- fighter.js
+++ fighter.js
@@ -1,1 +1,36 @@
-function 
+// function showNumbers(limit){
+//     let even = true;
+//     for (let i = 0; i <= limit; i++){
+//         if (even == true){
+//             console.log("EVEN");
+//             even = false;
+//         }
+//         else {
+//             console.log("ODD");
+//             even = true;
+//         }
+//     }
+// }
+
+// function showNumbers(limit){
+//     for (let i = 0; i <= limit; i++){
+//         console.log(i % 2 == 0 ? "EVEN" : "ODD");
+//     }
+// }
+
+// showNumbers(10);
+
+const array = [0,null, undefined, '', NaN, 10, 5, 11];
+console.log(countTruthy(array));
+
+function countTruthy(array){
+    for (let element of array){
+        if (element){
+            console.log(true);
+        }
+        else {
+            console.log(false);
+        }
+    }
+    //Used this one cause for of is better to iterate over elemtns
+}
